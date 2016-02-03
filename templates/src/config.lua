77b26350e7703b256b5f620e426753836338c0a3
nmonterroso <nelsonmonterroso@gmail.com>
2016-02-03 14:54:19 -0800

    change /token implementation to make a request to the same upstream that's defined for authentication

--- templates/src/config.lua
+++ templates/src/config.lua
@@ -2,7 +2,7 @@
 --
 local config = {
   SERVICE_LB_URL = "{{key "config/api-gateway/SERVICE_LB_URL"}}",
-  HELIOS_URL = "{{key "config/api-gateway/HELIOS_URL"}}",
+  AUTH_UPSTREAM_NAME = "{{key_or_default "config/api-gateway/AUTH_UPSTREAM_NAME" "auth"}},
   SERVICE_HTTP_TIMEOUT = 0.1,
 }
 
