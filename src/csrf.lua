d98e6c65e3cace0a59ef60f690b44b0c14050aef
Paweł Wójcik <pwojcik@wikia-inc.com>
2016-07-21 13:46:09 +0200

    log potential csrf origin

--- src/csrf.lua
+++ src/csrf.lua
@@ -4,10 +4,11 @@ local csrf = {}
 
 function csrf.protect_cookie_from_cross_origin_requests(ngx)
   local origin = cors.get_origin(ngx)
-  
+
   if origin ~= nil then -- perform the check only when origin header is present
     if not cors.origin_matches_whitelist(origin) then
         ngx.req.set_header(cookie.COOKIE_HEADER, "")
+        ngx.var.potential_csrf_origin = origin
     end
   end
   -- when there is no Origin header, it means that the request isn't coming from the browser and thus can be safly handled as is
