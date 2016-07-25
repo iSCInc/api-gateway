6ea3bfc5bca45a2ed09957a507eb5aa43e494c98
Paweł Wójcik <pwojcik@wikia-inc.com>
2016-07-25 15:23:43 +0200

    partial origin check

--- src/csrf.lua
+++ src/csrf.lua
@@ -5,13 +5,13 @@ local csrf = {}
 function csrf.protect_cookie_from_cross_origin_requests(ngx)
   local origin = cors.get_origin(ngx)
 
-  if origin ~= nil then -- perform the check only when origin header is present
+  if origin ~= nil and ngx.var.check_origin == "yes" then -- perform the check only when origin header is present
     if not cors.origin_matches_whitelist(origin) then
         ngx.req.set_header(cookie.COOKIE_HEADER, "")
         ngx.var.potential_csrf_origin = origin
     end
   end
-  -- when there is no Origin header, it means that the request isn't coming from the browser and thus can be safly handled as is
+  -- when there is no Origin header, it means that the request isn't coming from the browser and thus can be safely handled as is
 end
 
 return csrf
